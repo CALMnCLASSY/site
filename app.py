d6514b9185747f94ca2b9fd01d1c815675d8cade
CALMnCLASSY <calmnclassydev@gmail.com>
2025-03-27 14:48:59 +0300

    Redesigned navigation and footer for better UX

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ print("your_app_module.py: Starting...")
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 print("your_app_module.py: Flask imported.")
 from flask_mail import Mail, Message
+from bs4 import BeautifulSoup
 import feedparser
 
 app = Flask(__name__)
@@ -17,6 +18,11 @@ app.config['MAIL_PASSWORD'] = 'orbn gmyr ecpe fkgo'
 mail = Mail(app)
 RSS_FEED_URL = 'https://techcrunch.com/feed/'
 
+def extract_image(description):
+    soup = BeautifulSoup(description, 'html.parser')
+    img = soup.find('img')
+    return img['src'] if img else None
+
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -24,7 +30,11 @@ def home():
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    feed = feedparser.parse('https://www.theverge.com/rss/index.xml')
+    if 'entries' in feed:
+        return render_template('about.html', feed=feed.entries[:3])
+    else:
+        return render_template('about.html', feed=[])  # Pass an empty list if no entries
 
 
 @app.route('/services')
@@ -57,6 +67,19 @@ def send_message():
 def rss():
     feed = feedparser.parse(RSS_FEED_URL)
     return render_template('rss.html', feed=feed.entries)
+
+@app.route('/blog')
+def blog():
+    feed = feedparser.parse('https://www.theverge.com/rss/index.xml')
+    posts = []
+    for entry in feed.entries[:5]:  
+        posts.append({
+            'title': entry.title,
+            'link': entry.link,
+            'summary': entry.summary,
+            'image': extract_image(entry.description) 
+        })
+    return render_template('blog.html', posts=posts)
 
 
 if __name__ == '__main__':
